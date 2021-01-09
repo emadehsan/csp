47f0f1f8ed7bb61f61160c356a9d6eb455f4f0fa
Ben Lawrence <benlawraus@icloud.com>
2021-01-09 05:36:19 -0800

    Added typer

--- csp/read_lengths.py
+++ csp/read_lengths.py
@@ -1,10 +1,12 @@
 import pathlib
 from typing import List
 import re
+from math import ceil
 def get_data(infile:str)->List[float]:
+    """ Reads a file of numbers and returns a list of (count, number) pairs."""
     _p = pathlib.Path(infile)
     input_text = _p.read_text()
-    numbers = [round(float(n)) for n in re.findall(r'[0-9.]+', _p.read_text())]
+    numbers = [ceil(float(n)) for n in re.findall(r'[0-9.]+', _p.read_text())]
     quan = []
     nr = []
     for n in numbers:
